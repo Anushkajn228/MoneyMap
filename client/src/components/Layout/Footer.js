6149d74a8d6f3ae85ab694fac03fc86b1450467f
Anushkajn228 <163176454+Anushkajn228@users.noreply.github.com>
2024-10-18 21:25:27 +0530

    changes

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <h6 className="footer-text">All rights reserved &copy; Anushka Jain</h6>
+      <h6 className="footer-text">All rights reserved &copy;Anushka Jain</h6>
     </div>
   );
 };
